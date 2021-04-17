f5a9b02418837b51f0c4cd22aae44647a68a5b68
강효선 <kanghyoseon@ganghyoseon-ui-MacBook-Pro.local>
2021-04-17 14:34:42 +0900

    modify: question and solution

--- src/question/Members.js
+++ src/question/Members.js
@@ -9,24 +9,27 @@ import { cloneObject } from './util';
 * @description 생성자 함수로 최초 데이터를 받아서 virtual tree 구축과 함께 HTML node를 생성 및 저장한다.
 */
 function Members({ list }) {
-	// data를 저장하는 state 변수
-	let state = null;
 	// virtual dom tree
 	let virtualDomTree = null;
 	// html node
 	let htmlNode = null;
-	// use effect로 등록되는 callback
-	const effects = {};
+	// hook이 등록될때마다 state가 저장되는 변수.
+	const states = [];
+	// hook 실행 index
+	let currentState = 0;
+	// effect hook이 등록될때마다 effect가 저장되는 변수.
+	const effects = [];
+	// effect 실행 index
+	let currentEffect = 0;
 
 	/**
-	* @name updateState
+	* @name render
 	* @param {*} newState
-	* @description state 변경에 따른 virtual dom update
+	* @description state 변경에 따른 rendering
 	*/
-	const updateState = function (newState) {
-		// state를 업데이트한다.
-		state = Object.assign({}, state, newState);
-
+	const render = function (state) {
+		 // 새롭게 랜더링 하므로 초기화
+		currentState = currentEffect = 0;
 		// 변경된 state 정보를 가지고 tree 를 갱신한다.
 		const newVirtualDomTree = setVirtualTreeNode(state);
 		// 변경된 정보를 체크한다.
@@ -44,16 +47,20 @@ function Members({ list }) {
 	* @returns [현재값, 변경 함수]
 	* @description react useState함수와 같은 역할
 	*/
-	const useState = function (value) {
-		// 초기값 지정
-		if (!state) {
-			state = { value };
+	const useState = function (initialValue) {
+		states[currentState] = states[currentState] || initialValue;
+		// updateState 함수에서 currentState가 덮어 씌워지는 것을 방지.
+		const updateStateIndex = currentState;
+		const updateState = (newState) => {
+			states[updateStateIndex] = newState;
+			// data가 업데이트 되면 re render
+			render(states[updateStateIndex]);
 		}
 
 		// 두번째 값인 함수로 state를 변경해야만 값이 바뀌도록 한다.
 		return [
-			state.value,
-			list => updateState({ list })
+			states[currentState++],
+			updateState
 		];
 	}
 
@@ -64,14 +71,27 @@ function Members({ list }) {
 	* @description effect를 실행할 함수와 의존성 데이터를 array로 받는다.
 	*/
 	const useEffect = function (effect, deps) {
+		// q3. effect 함수가 반환함수가 있다면 (cleanup) effect 실행전에 실행하도록 작성하시오.
+		// TODO: Write JS code here!'
+		// effect를 실행하기전 cleanup이 등록이 되어 있으면 실행 후 초기화 한다.
+
 		// q1. effect를 실행함수와 의존성 데이터를 저장하는 useEffect 함수를 작성하시오.
 		// TODO: Write JS code here!'
-		if (deps) {
-			
-		} else {
-			
-		}
+		// effect 및 deps를 저장한다.
 	};
+
+	/**
+	* @name executeEffect
+	* @param {effect}
+	* @returns void
+	* @description rendering 후에 effect를 실행.
+	*/
+	function executeEffect(effect) {
+		// q2. 의존성 데이터 여부에 따라 effect를 실행하는 함수를 작성하시오.
+		// TODO: Write JS code here!'
+		// case 1) 의존성 데이터가 undefined일 경우에는 effect를 매번 실행
+		// case 2) 의존성 데이터가 있을 경우에는 데이터 변경 여부를 체크하여 effect를 실행
+	}
 
 	/**
 	* @name setVirtualTreeNode
@@ -79,26 +99,30 @@ function Members({ list }) {
 	* @returns virtual node
 	* @description list를 인자로 받아 data가 binding 된 virtual tree 구축 및 action 함수 정의.
 	*/
-	const setVirtualTreeNode = function ({ list }) {
+	const setVirtualTreeNode = function (list = []) {
 		const [members, setMembers] = useState(list);
 
 		useEffect(() => {
-			// q2. effect 함수에는 document의 title에 데이터의 갯수를 표현하여 변경을 확인할 수 있도록 작성하시오.
+			// q4. effect 함수에는 document의 title에 데이터의 갯수를 표현하여 변경을 확인할 수 있도록 작성하시오.
 			// TODO: Write JS code here!'
-		});
+		}, [members]);
+		// (참고: object-hash라이브러리를 통해서 매번 새로 생성되는 object 값의 변경여부를 쉽게 파악할 수 있음)
 
 		const onKeyUp = (event) => {
 			if (window.event.keyCode === 13) {
 				members.push(event.target.value);
 				event.target.value = '';
-				setMembers(members);
+				// 새로운 데이터로 넣어줌.
+				setMembers(cloneObject(members));
 			}
 		};
 
 		const addRow = () => {
-			members.push(htmlNode.querySelector('#memberInput').value);
-			htmlNode.querySelector('#memberInput').value = '';
-			setMembers(members);
+			const inputElement = htmlNode.querySelector('#memberInput');
+			members.push(inputElement.value);
+			inputElement.value = '';
+			// 새로운 데이터로 넣어줌.
+			setMembers(cloneObject(members));
 		}
 
 		return h('div', { style: 'width: 100%; margin-top: 10px' }, [
@@ -117,31 +141,22 @@ function Members({ list }) {
 	function addEvent(htmlNode) {
 		// 해당 이벤트로 dom update 변경을 감지하도록 한다. why? effect는 렌더링후에 실행되기 때문.
 		htmlNode.addEventListener('DOMNodeInserted', (event) => {
-			for (const effect in effects) {
-				executeEffect(effects[effect]);
-			}
+			effects.forEach((effect) => {
+				executeEffect(effect);
+			});
 		});
-		// element가 삭제되면 cleanup을 실행한다.
+		// element가 삭제되면 (unmount 시점) cleanup을 실행한다.
 		htmlNode.addEventListener ('DOMNodeRemovedFromDocument', (event) => {
-			for (const effect in effects) {
-				effects[effect].cleanup();
-			}
+			effects.forEach((effect) => {
+				if (effect.cleanup) {
+					effect.cleanup();
+				}
+			});
 		}, false);
 	}
 
-	function executeEffect(effect) {
-		// q4. effect 함수가 반환함수가 있다면 (cleanup) effect 실행전에 실행하도록 작성하시오.
-		// TODO: Write JS code here!'
-
-		// q3. 의존성 데이터 여부에 따라 effect를 실행하는 함수를 작성하시오.
-		// TODO: Write JS code here!'
-		// case 1) 의존성 데이터가 undefined일 경우에는 effect를 매번 실행
-		// case 2) 의존성 데이터가 있을 경우에는 데이터 변경 여부를 체크하여 effect를 실행 
-		// (참고: object-hash라이브러리를 통해서 변경여부를 쉽게 파악할 수 있음)
-	}
-
 	// virtual tree node를 갱신
-	virtualDomTree = setVirtualTreeNode({ list });
+	virtualDomTree = setVirtualTreeNode(list);
 	//virtual tree node를 html node로 생성
 	htmlNode = create(virtualDomTree);
 	addEvent(htmlNode);
--- src/solution/presenter/Members.js
+++ src/solution/presenter/Members.js
@@ -9,24 +9,27 @@ import { cloneObject } from './util';
 * @description 생성자 함수로 최초 데이터를 받아서 virtual tree 구축과 함께 HTML node를 생성 및 저장한다.
 */
 function Members({ list }) {
-	// data를 저장하는 state 변수
-	let state = null;
 	// virtual dom tree
 	let virtualDomTree = null;
 	// html node
 	let htmlNode = null;
-	// use effect로 등록되는 callback
-	const effects = {};
+	// hook이 등록될때마다 state가 저장되는 변수.
+	const states = [];
+	// hook 실행 index
+	let currentState = 0;
+	// effect hook이 등록될때마다 effect가 저장되는 변수.
+	const effects = [];
+	// effect 실행 index
+	let currentEffect = 0;
 
 	/**
-	* @name updateState
+	* @name render
 	* @param {*} newState
-	* @description state 변경에 따른 virtual dom update
+	* @description state 변경에 따른 rendering
 	*/
-	const updateState = function (newState) {
-		// state를 업데이트한다.
-		state = Object.assign({}, state, newState);
-
+	const render = function (state) {
+		 // 새롭게 랜더링 하므로 초기화
+		currentState = currentEffect = 0;
 		// 변경된 state 정보를 가지고 tree 를 갱신한다.
 		const newVirtualDomTree = setVirtualTreeNode(state);
 		// 변경된 정보를 체크한다.
@@ -44,16 +47,20 @@ function Members({ list }) {
 	* @returns [현재값, 변경 함수]
 	* @description react useState함수와 같은 역할
 	*/
-	const useState = function (value) {
-		// 초기값 지정
-		if (!state) {
-			state = { value };
+	const useState = function (initialValue) {
+		states[currentState] = states[currentState] || initialValue;
+		// updateState 함수에서 currentState가 덮어 씌워지는 것을 방지.
+		const updateStateIndex = currentState;
+		const updateState = (newState) => {
+			states[updateStateIndex] = newState;
+			// data가 업데이트 되면 re render
+			render(states[updateStateIndex]);
 		}
 
 		// 두번째 값인 함수로 state를 변경해야만 값이 바뀌도록 한다.
 		return [
-			state.value,
-			list => updateState({ list })
+			states[currentState++],
+			updateState
 		];
 	}
 
@@ -64,26 +71,57 @@ function Members({ list }) {
 	* @description effect를 실행할 함수와 의존성 데이터를 array로 받는다.
 	*/
 	const useEffect = function (effect, deps) {
-		// 최초 effect 및 deps를 저장한다.
-		if (deps) {
-			// deps가 있을 경우에는 데이터 체크를 위해 저장소를 준비.
-			if (!effects[effect]) {
-				effects[effect] = {
-					effect,
-					prevDeps: undefined, // 이전 의존성 데이터.
-					newDeps: undefined, // 새로운 의존성 데이터.
-					cleanup: undefined, // cleanup을 실행할 함수.
-				};
-			}
-			effects[effect].newDeps = cloneObject(deps);
+		// effect를 실행하기전 cleanup이 등록이 되어 있으면 실행 후 초기화 한다.
+		if (effects[currentEffect] && effects[currentEffect].cleanup) {
+			effects[currentEffect].cleanup();
+			effects[currentEffect].cleanup = undefined;
+		}
+
+		// effect 및 deps를 저장한다.
+		if (!effects[currentEffect]) {
+			effects[currentEffect] = {
+				effect: undefined, // effect 함수
+				prevDeps: undefined, // 이전 의존성 데이터.
+				newDeps: undefined, // 새로운 의존성 데이터.
+				cleanup: undefined, // cleanup을 실행할 함수.
+			};
+		}
+		effects[currentEffect].newDeps = deps;
+		effects[currentEffect].effect = effect;
+		// 다음 hook을 위해
+		currentEffect++;
+	};
+
+	/**
+	* @name executeEffect
+	* @param {effect}
+	* @returns void
+	* @description rendering 후에 effect를 실행.
+	*/
+	function executeEffect(effect) {
+		// case 1) 의존성 데이터가 undefined일 경우에는 effect를 매번 실행
+		// case 2) 의존성 데이터가 있을 경우에는 데이터 변경 여부를 체크하여 effect를 실행 
+		if (!effect.newDeps) {
+			// deps가 undefinde 일 경우 effect를 항상 실행.
+			const cleanup = effect.effect();
+			effect.cleanup = cleanup;
 		} else {
-			// deps가 없을 경우에는 effect만 필요
-			if (!effects[effect]) {
-				effects[effect] = {};
+			// 최초 effect 실행
+			if (!effect.prevDeps) {
+				const cleanup = effect.effect();
+				effect.cleanup = cleanup;
+				effect.prevDeps = effect.newDeps;
+				return;
+			}
+			const isChangedDeps = !effect.newDeps.every((el, i) => el === effect.prevDeps[i]);
+			// deps가 빈 배열이라면 최초한번만 실행이 된다. why? 체크해야할 의존성 데이터가 없기 때문에
+			if (isChangedDeps) {
+				effect.prevDeps = effect.newDeps;
+				const cleanup = effect.effect();
+				effect.cleanup = cleanup;
 			}
 		}
-		effects[effect].effect = effect;
-	};
+	}
 
 	/**
 	* @name setVirtualTreeNode
@@ -91,7 +129,7 @@ function Members({ list }) {
 	* @returns virtual node
 	* @description list를 인자로 받아 data가 binding 된 virtual tree 구축 및 action 함수 정의.
 	*/
-	const setVirtualTreeNode = function ({ list }) {
+	const setVirtualTreeNode = function (list = []) {
 		const [members, setMembers] = useState(list);
 
 		useEffect(() => {
@@ -100,19 +138,23 @@ function Members({ list }) {
 				document.title = 'React의 useEffect 따라해보기.';
 			}
 		}, [members]);
+		// (참고: object-hash라이브러리를 통해서 매번 새로 생성되는 object 값의 변경여부를 쉽게 파악할 수 있음)
 
 		const onKeyUp = (event) => {
 			if (window.event.keyCode === 13) {
 				members.push(event.target.value);
 				event.target.value = '';
-				setMembers(members);
+				// 새로운 데이터로 넣어줌.
+				setMembers(cloneObject(members));
 			}
 		};
 
 		const addRow = () => {
-			members.push(htmlNode.querySelector('#memberInput').value);
-			htmlNode.querySelector('#memberInput').value = '';
-			setMembers(members);
+			const inputElement = htmlNode.querySelector('#memberInput');
+			members.push(inputElement.value);
+			inputElement.value = '';
+			// 새로운 데이터로 넣어줌.
+			setMembers(cloneObject(members));
 		}
 
 		return h('div', { style: 'width: 100%; margin-top: 10px' }, [
@@ -131,53 +173,22 @@ function Members({ list }) {
 	function addEvent(htmlNode) {
 		// 해당 이벤트로 dom update 변경을 감지하도록 한다. why? effect는 렌더링후에 실행되기 때문.
 		htmlNode.addEventListener('DOMNodeInserted', (event) => {
-			for (const effect in effects) {
-				executeEffect(effects[effect]);
-			}
+			effects.forEach((effect) => {
+				executeEffect(effect);
+			});
 		});
-		// element가 삭제되면 cleanup을 실행한다.
+		// element가 삭제되면 (unmount 시점) cleanup을 실행한다.
 		htmlNode.addEventListener ('DOMNodeRemovedFromDocument', (event) => {
-			for (const effect in effects) {
-				effects[effect].cleanup();
-			}
+			effects.forEach((effect) => {
+				if (effect.cleanup) {
+					effect.cleanup();
+				}
+			});
 		}, false);
 	}
 
-	function executeEffect(effect) {
-		// effect를 실행하기전 cleanup이 등록이 되어 있으면 실행한다.
-		if (effect.cleanup) {
-			effect.cleanup();
-		}
-		// 최초 실행.
-		if (!effect.newDeps) {
-			// deps가 undefinde 일 경우 effect를 항상 실행.
-			const cleanup = effect.effect();
-			effect.cleanup = cleanup;
-		} else {
-			// 최초 effect 실행
-			if (!effect.prevDeps) {
-				const cleanup = effect.effect();
-				effect.cleanup = cleanup;
-				effect.prevDeps = effect.newDeps;
-				return;
-			}
-			// deps가 빈 배열이라면 최초한번만 실행이 된다. why? 체크해야할 의존성 데이터가 없기 때문에
-			// 자세한 내용은 https://rinae.dev/posts/a-complete-guide-to-useeffect-ko 참조
-			for (let index = 0; index < effect.newDeps.length; index++) {
-				const arg = effect.newDeps[index];
-				if (arg !== effect.prevDeps[index]) {
-					// 변경에 대한 상태를 업데이트 한다.
-					effect.prevDeps[index] = arg;
-					const cleanup = effect.effect();
-					effect.cleanup = cleanup;
-					break;
-				}
-			}
-		}
-	}
-
 	// virtual tree node를 갱신
-	virtualDomTree = setVirtualTreeNode({ list });
+	virtualDomTree = setVirtualTreeNode(list);
 	//virtual tree node를 html node로 생성
 	htmlNode = create(virtualDomTree);
 	addEvent(htmlNode);
